be7c564ec0df86ae295958dcd4d94bb4464c469c
Marko_TON <markotoncanovic@gmail.com>
2022-05-21 16:31:47 +0200

    chenging order and adding comments

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,17 @@
 var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
 // character = 20;
 character = 'luigi';
+var age = 30;
 // age = 'yoshi';
 age = 40;
+var isBlackBelt = false;
 // isBlackBelt = 'yes';
 isBlackBelt = true;
-var area = function (diameter) {
+var circ = function (diameter) {
     return diameter * Math.PI;
 };
-// console.log(area('hello'));
-console.log(area(7.5));
+// const area = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
+// console.log(area('hello')); // error string
+console.log(circ(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,12 @@
 let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
-
 // character = 20;
 character = 'luigi';
 
+let age = 30;
 // age = 'yoshi';
 age = 40;
 
+let isBlackBelt = false;
 // isBlackBelt = 'yes';
 isBlackBelt = true;
 
@@ -15,5 +14,9 @@ const circ = (diameter: number) => {
   return diameter * Math.PI;
 };
 
-// console.log(area('hello'));
+// const area = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
+// console.log(area('hello')); // error string
+
 console.log(circ(7.5));
