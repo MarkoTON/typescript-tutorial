899e90bcb75510f15c88b753db2651acc88a3d1a
Marko_TON <markotoncanovic@gmail.com>
2022-05-21 16:46:23 +0200

    adding comments

--- sandbox.ts
+++ sandbox.ts
@@ -11,7 +11,7 @@ numbers.push(25);
 // numbers.push('shaun');
 // numbers[0] = 'shaun';
 
-let mixed = ['ken', 4, 'chun-li', 8, 9];
+let mixed = ['ken', 4, 'chun-li', 8, 9]; // string | number - nece biti 'any'
 
 mixed.push('ryu');
 mixed.push(10);
@@ -21,17 +21,20 @@ mixed[0] = 3;
 let ninja = {
   name: 'mario',
   belt: 'black',
-  age: 30
+  age: 30 as number | string
 };
 
 ninja.age = 40;
 ninja.name = 'ryu';
-// ninja.age = '30';
+ninja.age = '30';
+// Ne moze da se doda novi properti
 // ninja.skills = ['fighting', 'sneaking']
 
 ninja = {
+  // I ne moze da se menja type, mora da ostane kao sto je bio, samo se menja vrednost
   name: 'yoshi',
   belt: 'orange',
-  age: 40,
-  // skills: ['running'],
+  age: 40, // nece ti dozvoliti ni da smanjis ili ti uklonis neki od propertija
+  // Ne moze da se doda novi properti
+  // skills: ['running']
 };
