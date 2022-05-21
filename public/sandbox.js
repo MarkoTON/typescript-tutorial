4de477abccf5f95201356fdf5e114461cf0d9f03
Marko_TON <markotoncanovic@gmail.com>
2022-05-21 17:54:11 +0200

    checking functions

--- public/sandbox.js
+++ public/sandbox.js
@@ -6,14 +6,20 @@
 // greet = () => {
 //   console.log('hello, again');
 // }
+// kada se stavi ? onda je opciono -> "c?: number | string = 10"
 var add = function (a, b, c /*?*/) {
     if (c === void 0) { c /*?*/ = 10; }
     console.log(a + b);
+    console.log('---------------------');
     console.log(c);
 };
 add(5, 10, 'ninja');
 var minus = function (a, b) {
+    // Ako zelimo da vratiomo rezultat da bude String, a ne number
+    // const minus = (a: number, b: number): string => {
     return a + b;
+    // return String(a + b);
 };
+console.log('==========================');
 var result = minus(10, 7);
 console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -8,16 +8,22 @@
 //   console.log('hello, again');
 // }
 
+// kada se stavi ? onda je opciono -> "c?: number | string = 10"
 const add = (a: number, b: number, c/*?*/: number | string = 10): void => {
   console.log(a + b);
+  console.log('---------------------')
   console.log(c);
 }
 
 add(5, 10, 'ninja');
 
 const minus = (a: number, b: number): number => {
+  // Ako zelimo da vratiomo rezultat da bude String, a ne number
+  // const minus = (a: number, b: number): string => {
   return a + b;
+  // return String(a + b);
 }
 
+console.log('==========================')
 let result = minus(10,7);
 console.log(result);
