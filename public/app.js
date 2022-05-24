53765b15bc09e1f7f7fbb52d01e95703ec9f068a
Marko_TON <markotoncanovic@gmail.com>
2022-05-24 18:47:10 +0200

    adding comment

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-import { Invoice } from './classes/Invoice.js';
+import { Invoice } from './classes/Invoice.js'; // mora da bude .js a ne .ts jer ce na kraju da imporutje JavaScript, a ne Typescript
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 // invOne.client = 'yoshi';
